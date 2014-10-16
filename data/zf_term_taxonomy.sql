7e02170925f958d1deb44099ffebb0bee5935874
Phan <phanvanthanhda10tt@gmail.com>
2014-10-16 21:09:59 +0700

    them file csdl kiem thu

---
+++ data/zf_term_taxonomy.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO `zf_term_taxonomy` (`term_taxonomy_id`, `term_id`, `taxonomy`, `description`, `parent`, `count`) VALUES
+(45, 0, 'a', NULL, NULL, NULL),
+(46, 0, 'b', NULL, NULL, NULL),
+(47, 2, 'b', NULL, 46, NULL),
+(48, 3, 'b', NULL, 47, NULL),
+(49, 4, 'b', NULL, 46, NULL);
